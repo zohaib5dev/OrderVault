767f03ec9655213f4ee1ec447721aa50ef5fb63d
zohaib5laravel-code <zohaib5laravel@gmail.com>
2026-06-29 06:34:24 +0500

    updated for single OrderVault

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,11 @@
 <?php
-
-use App\Http\Controllers\Api\Vendor\WebhookController;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\Vendor\SubscriptionController;
+ 
+use App\Http\Controllers\ProfileController; 
 use Illuminate\Support\Facades\Route;
 
 Route::get('/{any}', function () {
     return view('app');
 })->where('any', '.*');
-
-
-Route::get('/stripe/refresh', [SubscriptionController::class, 'refreshStripeOnboarding'])->name('stripe.refresh');
-Route::get('/stripe/return', [SubscriptionController::class, 'handleStripeReturn'])->name('stripe.return');
-Route::get('/stripe/success', [SubscriptionController::class, 'handleStripeSuccess'])->name('stripe.success');
-
-Route::get('/stripe/handle-webhook', [WebhookController::class, 'handleStripeWebhook'])->name('stripe.handle-webhook');
 
 
 Route::middleware('auth')->group(function () {
